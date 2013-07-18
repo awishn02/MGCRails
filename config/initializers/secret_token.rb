da3384ea73f713fe9792e3b0bedc562438a2a3bb
Aaron Wishnick <Aaron_B.Wishnick@tufts.edu>
2013-07-18 19:54:39 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MGC::Application.config.secret_token = '3ae55d6ddc565123c7de85966fba52565d5b4f32e0ed292abef4e8c20c75140a3f67c1ba87ad0998f04df6aa7b50ddb617cce21a595a332985572ef3d3ebd2b7'
