5175badbd8a1b897096d5689f87522472ed227df
Aaron Wishnick <Aaron_B.Wishnick@tufts.edu>
2013-07-18 21:41:35 -0400

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 MGC::Application.routes.draw do
+  get "home/index"
+
   resources :ongoings
 
   resources :locations
 
   resources :games
+  
+  root to: 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
